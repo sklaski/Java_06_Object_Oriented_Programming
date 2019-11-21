00903310a00dbb2fd85af6eb1e47bf83d021b2e3
Steffen Klaski <steffen.klaski@t-systems.com>
2019-11-21 19:50:59 +0100

    Corrections: System.out.print in Classroom more getter/setter

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,16 +1,33 @@
 public class Classroom {
 
 	public static void main(String[] args) {
+		//some creates
 		Wilder person1 = new Wilder("Steffen", true);
 		Wilder person2 = new Wilder("Arne");
 		Wilder person3 = new Wilder("Metje", false);
-		person1.setIfPresent(false);
-		person2.setIfPresent(true);
-		person1.whoAmI();
-		person2.whoAmI();
-		person3.whoAmI();
-		System.out.println(new Wilder("Ralf", false));
-//		System.out.println("number of elements: " + person2.getCounter());
+		
+		//present settings/changes
+		person1.setPresent(false);
+		person2.setPresent(true);
+		
+		System.out.println(person3.whoAmI()); //whoamI
+		person3.setName("Stefi"); //name correction
+		
+		//more whoamI's
+		System.out.println(person1.whoAmI());
+		System.out.println(person2.whoAmI());
+		System.out.println(person3.getName()); //get only name
+		
+		//isPresent? start
+		System.out.print("Steffen is "); 
+		if (!person1.isPresent()) {
+			System.out.print("not ");
+		}
+		System.out.println("present!");
+		//isPresent? end
+		
+		System.out.println(new Wilder("Ralf", false)); //useless address!?
+		
 	}
 
 }
--- src/Wilder.java
+++ src/Wilder.java
@@ -1,88 +1,43 @@
+import java.util.ArrayList;
+
 public class Wilder {
 
 	private String firstname;
 	private boolean present;
-	private int counter = 0;
-//	
-//	public int getCounter() {
-//		return counter;
-//	}
-//	
-//	public void incCounter() {
-//		counter++;
-//	}
-//	
+	
 	public Wilder(String firstname) {
 		this.firstname = firstname;
-//		incCounter();
 	}
 	
 	public Wilder(String firstname, boolean present) {
 		this.firstname = firstname;
 		this.present = present;
-//		incCounter();
 	}
+
 	
-	public void whoAmI() {
-		System.out.print("My name is " + firstname + " and I am ");
+	public String whoAmI() {
+		String out = "My name is " + firstname + " and I am ";
 		if (!present) {
-			System.out.print("not ");
+			out += "not ";
 		}
-		System.out.println("present");
+		out += "present";
+		return out;
 	}
 	
-	public void setIfPresent(boolean present) {
+	public boolean isPresent() {
+		return this.present;
+	}
+	
+	public void setPresent(boolean present) {
 		this.present = present;
 	}
+
+	public void setName(String newName) {
+		this.firstname = newName;
+	}
+
+	public String getName() {
+		return firstname;
+	}
 	
 }
-
-	/*   
-		// TODO Auto-generated method stub
-			
-		private String firstname;
-		private boolean present;
-		
-		public Wilder (String firstname) {
-			this.firstname = firstname;
-			this.present = false;
-		}
-		
-		public Wilder (String firstname, boolean present) {
-			this.firstname = firstname;
-			this.present = present;
-		}
-}
-
-
- 1.Create a Wilder class
- 2.Adds the attributes String firstname and boolean present
- 3.Create a constructor to initialise firstname and present 
- 4.Add getters and setters for all attributes, according to the conventions
- 5.Add a whoAmI() method that returns the text "My name is firstname and I am present" or "My name is firstname and I am not present", depending on the state of the attributes firstname and present 
- 6.Create a Classroom class with a main() method
- 7.Add references to several instances of the Wilder class inside the main() method of Classroom
- 8.For each of the references, display the result of the whoAmI() method in the terminal
- 9.Compile and test Classroom: once everything is working perfectly, create a git repository and send it all to GitHub
-10.Share the link to the GitHub repository as your solution
-		
-		Example of the expected result when running Classroom:
-		My name is Jean-Claude and I am present
-		My name is Henri and I'm not present
-
-Validation criteria :
-
-•
-
-Your GitHub repository contains the files Wilder.java and Classroom.java
-•
-
-The Wilder class follows OOP conventions: private attributes, using the keyword this, and contains appropriately named getters and setters.
-•
-
-References to instances of the Wilder class are present within the Classroom class
-•
-
-The Classroom class compiles without throwing any errors and displays the expected result in the terminal.
-
-		*/	
